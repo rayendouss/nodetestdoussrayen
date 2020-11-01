3a703f1d2f03e71d5ca8e9ab9b452ae221c754e9
unknown <mohamedrayane.douss@esprit.tn>
2020-11-01 21:27:45 +0100

    deploy4

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 
-var PORT=5000 || process.env.PORT;
+var PORT=process.env.PORT || 5000;
 var rateLimit = [];
 const connectDB = async () => {
     try{
